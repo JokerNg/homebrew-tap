0f1eaa775090f175df5744128b477a098267ab3e
JokerNg <jokerng@126.com>
2026-09-14 16:29:38 +0800

    chore: bump codex-touchbar-monitor to 0.1.17

--- Casks/codex-touchbar-monitor.rb
+++ Casks/codex-touchbar-monitor.rb
@@ -1,6 +1,6 @@
 cask "codex-touchbar-monitor" do
-  version "0.1.16"
-  sha256 "d854bf48179f8a1ffc8fba6646b9a889985f5a43136915afb4f404186ada9d64"
+  version "0.1.17"
+  sha256 "f32ca164820f76846d2c102e9cd1619a29df6c992439851b09cd092f497f4847"
 
   url "https://github.com/JokerNg/CodexTouchBarMonitor/releases/download/v#{version}/CodexTouchBarMonitor-#{version}.dmg"
   name "CodexTouchBarMonitor"
